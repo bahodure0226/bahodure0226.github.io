7278e2384b66f34e15f9daab4c71e300628243bf
bahodure0226 <bahodure@nycstudents.net>
2024-09-27 13:15:22 -0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Bahodur Ergashev"; // update the part BETWEEN the "quotes"
+let userName = "bahodure0226"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
